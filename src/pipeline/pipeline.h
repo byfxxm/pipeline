c79dd52e5c571f1a7f3768a8680a773b24a55b6a
byfxxm <byfxxm@outlook.com>
2021-11-22 18:49:36 +0800

    22

--- src/pipeline/pipeline.h
+++ src/pipeline/pipeline.h
@@ -7,20 +7,20 @@
 #define PIPELINE_API __declspec(dllimport)
 #endif
 
+struct part {};
+using read_func = part * (*)();
+using write_func = void(*)(part*);
+using output_func = write_func;
+	
+struct utility
+{
+	read_func read{ nullptr };
+	write_func write{ nullptr };
+};
+using procedure_func = void(*)(utility*);
+
 extern "C"
 {
-	struct part {};
-	using read_func = part * (*)();
-	using write_func = void(*)(part*);
-	using output_func = write_func;
-	
-	struct utility
-	{
-		read_func read{ nullptr };
-		write_func write{ nullptr };
-	};
-	using procedure_func = void(*)(utility*);
-
 	PIPELINE_API void* pipeline_create();
 	PIPELINE_API void pipeline_delete(void* pipeline);
 	PIPELINE_API void pipeline_start(void* pipeline, output_func output);
