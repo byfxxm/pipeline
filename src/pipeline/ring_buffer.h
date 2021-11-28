7f60951537784e78c1c7c8fbd85646350cd34220
byfxxm <byfxxm@outlook.com>
2021-11-28 08:19:54 +0800

    22

--- src/pipeline/ring_buffer.h
+++ src/pipeline/ring_buffer.h
@@ -9,8 +9,8 @@ public:
 		if (is_full())
 			return false;
 
-		__buffer[__tail] = data;
-		__tail = (__tail + 1) % N;
+		__buffer[__write_index] = data;
+		__write_index = (__write_index + 1) % N;
 		return true;
 	}
 
@@ -19,28 +19,28 @@ public:
 		if (is_empty())
 			return false;
 
-		data = __buffer[__head];
-		__head = (__head + 1) % N;
+		data = __buffer[__read_index];
+		__read_index = (__read_index + 1) % N;
 		return true;
 	}
 
 	bool is_empty()
 	{
-		return __head == __tail;
+		return __read_index == __write_index;
 	}
 
 	bool is_full()
 	{
-		return (__tail + 1) % N == __head;
+		return (__write_index + 1) % N == __read_index;
 	}
 
 	void clear()
 	{
-		__head = __tail;
+		__read_index = __write_index;
 	}
 
 private:
 	T __buffer[N];
-	volatile unsigned __head{ 0 };
-	volatile unsigned __tail{ 0 };
+	volatile unsigned __read_index{ 0 };
+	volatile unsigned __write_index{ 0 };
 };
